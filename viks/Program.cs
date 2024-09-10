127f366d56219cec00c35f2dba0d6d99448828cb
vika <malyovanavika@gmail.com>
2024-09-10 11:50:10 +0300

    da

--- viks/Program.cs
+++ viks/Program.cs
@@ -4,10 +4,21 @@
 //calculate
 //result
 
+Console.WriteLine("---------Convert Miles to Km---------");
 Console.Write("Enter miles");
 
 float miles = float.Parse (Console.ReadLine());
 
 Console.WriteLine($"Result: {miles * 1.60934} km");
+
+
+
+Console.WriteLine("---------Circle Area by Radius---------");
+Console.Write("Enter radius: ");
+
+float r = float.Parse(Console.ReadLine());
+double area = Math.PI * Math.Pow(r, 2);  
+
+Console.WriteLine($"Result: {area} * cm`2 ");
 
 
