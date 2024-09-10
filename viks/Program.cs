c7fb79546578b1a6d5ced8d95baf8e3faee85b1c
vika <malyovanavika@gmail.com>
2024-09-10 11:49:42 +0300

    da

--- viks/Program.cs
+++ viks/Program.cs
@@ -1,2 +1,13 @@
-﻿Console.WriteLine("iyfiyf");
-Console.Write("lolkekchebyrek");
+﻿// милі в км
+
+//ask
+//calculate
+//result
+
+Console.Write("Enter miles");
+
+float miles = float.Parse (Console.ReadLine());
+
+Console.WriteLine($"Result: {miles * 1.60934} km");
+
+
